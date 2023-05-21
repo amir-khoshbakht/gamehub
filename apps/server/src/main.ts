8d60c827340ddfce4c5a42480628db6d66f7da01
amir-khoshbakht <amirrr.khoshbakht@gmail.com>
2023-05-21 20:12:37 +0330

    Fix class-transformer dependency

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, Logger } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   // TODO : move this to the .env file
   const port = 3000;
